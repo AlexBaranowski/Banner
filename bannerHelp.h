f729b4e78d433c759882083dbb1fc5dd75522f1a
Aleksander Baranowski <aleksander.baranowski@euro-linux.com>
2015-05-27 07:12:32 -0400

    Added GPL addnotation to help. Now ":" is printed little diffrently

--- bannerHelp.h
+++ bannerHelp.h
@@ -1,4 +1,9 @@
-char bannerHelpString [] = {"This is help for banner program \n\
+char bannerHelpString [] = {"\
+******************** This program is distributed in the hope that it will be useful, ********************\n\
+******************** but WITHOUT ANY WARRANTY; without even the implied warranty of ********************\n\
+******************** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the ********************\n\
+******************** GNU General Public License for more details. ********************\n\
+This is help for banner program \n\
 You can use this program in 2 ways\n\
 1) Without any option selected.\n\
 \t\"banner this is sentence\" will output as separate lines\n\
@@ -9,4 +14,4 @@ You can use this program in 2 ways\n\
 \t-s printing character is the same as letter\n\
 \t-i reads from stdin\n\
 \t-n no options print every line as it is try \"banner -s -n\" \"banner -n -s -n\"\n\
-This program is distributed "};
+"};
